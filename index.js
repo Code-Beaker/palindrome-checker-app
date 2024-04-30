56b81728beca379bc6c8fbb88351519e21c00de9
Tharun Raj <tharun.2.linux@gmail.com>
2024-04-30 14:36:32 +0530

    app completed!

---
+++ index.js
@@ -0,0 +1,22 @@
+const input = document.getElementById("wordInput");
+const resultStatus = document.getElementById("status");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function check() {
+  const value = input.value;
+  const reverse = reverseString(value);
+  alert(reverse);
+
+  if (reverse === value) {
+    console.log(true);
+    resultStatus.textContent = "Palindrome!";
+    resultStatus.classList.add("pop");
+  } else {
+    console.log(false);
+    resultStatus.textContent = "Not quite there!";
+    resultStatus.classList.remove("pop");
+  }
+}
